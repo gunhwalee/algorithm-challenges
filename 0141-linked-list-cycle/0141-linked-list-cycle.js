160f174510ebd62c15327ad0029ea91e24631401
Gunhwa Lee <110829006+gunhwalee@users.noreply.github.com>
2023-06-22 12:08:12 +0900

    Time: 78 ms (33.55%), Space: 45.5 MB (26.55%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycle = function(head) {
+  let fast = head;
+  
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    head = head.next;
+    
+    if (fast === head) return true;
+  }
+  
+  return false;
+};
