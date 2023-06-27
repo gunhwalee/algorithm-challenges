5c9dea47defba178977f1eb2296e08cdee21320a
Gunhwa Lee <110829006+gunhwalee@users.noreply.github.com>
2023-06-27 14:27:00 +0900

    Time: 65 ms (45.42%), Space: 43.8 MB (45.80%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.js
@@ -0,0 +1,26 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+const cloneGraph = function(node) {
+  // solution
+  const map = new Map();
+  const traverse = root => {
+    if (!map.has(root.val)) {
+      map.set(root.val, new Node(root.val));
+      map.get(root.val).neighbors = root.neighbors.map(traverse);
+    }
+    
+    return map.get(root.val);
+  }
+  
+  return node ? traverse(node) : node;
+};
