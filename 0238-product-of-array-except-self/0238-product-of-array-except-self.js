b6f19b60ea907e041a403cdfcbc1840e98e74912
Gunhwa Lee <110829006+gunhwalee@users.noreply.github.com>
2023-07-04 21:13:52 +0900

    Time: 97 ms (88.12%), Space: 55.5 MB (32.06%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,20 +3,17 @@
  * @return {number[]}
  */
 const productExceptSelf = function(nums) {
-  const leftArray = [];
-  const rightArray = [];
   const result = [];
   let leftNum = 1, rightNum = 1;
 
   for (let i = 0; i < nums.length; i += 1) {
-    leftArray[i] = leftNum;
+    result[i] = leftNum;
     leftNum *= nums[i];
   }
 
   for (let i = nums.length - 1; i >= 0; i -= 1) {
-    rightArray[i] = rightNum;
+    result[i] *= rightNum;
     rightNum *= nums[i];
-    result[i] = leftArray[i] * rightArray[i];
   }
 
   return result;
