913d3917174baaf95c24da02942e7340a4f8f8bb
Gunhwa Lee <110829006+gunhwalee@users.noreply.github.com>
2023-07-15 23:21:34 +0900

    Time: 72 ms (35.38%), Space: 44.7 MB (9.86%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,9 +11,10 @@
  * @return {ListNode}
  */
 const mergeTwoLists = function(list1, list2) {
-  if (!list1 || !list2) return list1 ? list1 : list2;
+  if (!list1) return list2;
+  if (!list2) return list1;
 
-  if (list1.val < list2.val) {
+  if (list1.val <= list2.val) {
     list1.next = mergeTwoLists(list1.next, list2);
     return list1;
   } else {
