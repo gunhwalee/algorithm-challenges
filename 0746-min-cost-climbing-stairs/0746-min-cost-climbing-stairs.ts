e7a01bb7220526f799857b1762ab2529a51d5ef4
Gunhwa Lee <110829006+gunhwalee@users.noreply.github.com>
2023-08-06 14:27:23 +0900

    Time: 56 ms (95.42%), Space: 45 MB (43.26%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.ts
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.ts
@@ -1,14 +1,15 @@
 const minCostClimbingStairs = (cost: number[]): number => {
-  let dp: number[] = new Array(cost.length);
+  const dp: number[] = new Array(cost.length);
+  cost.push(0);
   dp[0] = cost[0];
   dp[1] = cost[1];
   let n: number = 2;
   
-  while (n <= cost.length) {
-    dp[n] = cost[n] ? cost[n] + Math.min(dp[n - 1], dp[n - 2]) : Math.min(dp[n - 1], dp[n - 2]);
+  while (n < cost.length) {
+    dp[n] =cost[n] + Math.min(dp[n - 1], dp[n - 2]);
     
     n += 1;
   }
 
-  return dp[cost.length];
+  return dp[cost.length - 1];
 };
