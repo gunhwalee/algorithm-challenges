7e838a5f7d8be5b9bb1813f085b76190a75b6b25
Gunhwa Lee <110829006+gunhwalee@users.noreply.github.com>
2023-08-07 12:21:35 +0900

    Time: 57 ms (73.13%), Space: 42.7 MB (54.08%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.ts
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.ts
@@ -1,7 +1,5 @@
 const tribonacci = (n: number): number => {
-  const result = new Array(n + 1).fill(0);
-  result[1] = 1;
-  result[2] = 1;
+  const result: number[] = [0, 1, 1];
   
   for (let i = 3; i < n + 1; i += 1) {
     result[i] = result[i -3] + result[i - 2] + result[i - 1];
