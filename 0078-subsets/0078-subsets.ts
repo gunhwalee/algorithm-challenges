33e9f51612a5b65b137788293e78bdab6b5d1cf2
Gunhwa Lee <110829006+gunhwalee@users.noreply.github.com>
2023-08-12 15:02:23 +0900

    Time: 69 ms (43.15%), Space: 45.3 MB (38.59%) - LeetHub

--- 0078-subsets/0078-subsets.ts
+++ 0078-subsets/0078-subsets.ts
@@ -1,20 +1,17 @@
 const subsets = (nums: number[]): number[][] => {
-  const result: number[][] = [[]];
+  const result: number[][] = [];
 
-  const helper = (array, index) => {
-    if (index >= nums.length) return;
-    
-    array.push(nums[index]);
-    result.push(array);
-    
-    for (let i = index + 1; i < nums.length; i += 1) {
-      helper([...array], i);
+  const helper = (array: number[], index: number): void => {
+    if (index > nums.length) return;
+
+    result.push(array);    
+    for (let i = index; i < nums.length; i += 1) {
+      const newArray: number[] = [...array, nums[i]];
+      helper(newArray, i + 1);
     }
   };
-  
-  for (let i = 0; i < nums.length; i += 1) {
-    helper([], i);
-  }
+
+  helper([], 0);
 
   return result;
 };
