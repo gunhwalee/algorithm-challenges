fb7d4cc638a8430442318f349597288265d312f6
Gunhwa Lee <110829006+gunhwalee@users.noreply.github.com>
2023-08-15 13:19:50 +0900

    Time: 63 ms (50.70%), Space: 42.8 MB (81.41%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.ts
@@ -0,0 +1,11 @@
+const uniquePaths = (m: number, n: number): number => {
+  const dp = new Array(m).fill(new Array(n).fill(1));
+  
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  
+  return dp[m - 1][n - 1];
+};
